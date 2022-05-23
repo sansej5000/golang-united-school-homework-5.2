e4bba8d07aaf0641867aec901a61a171ada1aab8
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-05-23 16:12:27 +0300

    Update cache.go

--- cache.go
+++ cache.go
@@ -2,22 +2,51 @@ package cache
 
 import "time"
 
+type item struct {
+	value string
+	timer time.Time
+}
+
 type Cache struct {
+	storage map[string]item
 }
 
 func NewCache() Cache {
-	return Cache{}
+	storage := make(map[string]item)
+	return Cache{storage: storage}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (k Cache) Get(key string) (string, bool) {
+	item, ok := k.storage[key]
+	if !ok {
+		return "", false
+	}
+	if item.timer == 0 || time.Now().Before(item.timer) {
+		return item.value, true
+	}
+	delete(k.storage, key)
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (k Cache) Put(key, value string) {
+	k.storage[key] = item{value: value, timer:0}
+	//помещает значение со связанным ключом в кеш. Значение, введенное с помощью этого метода, никогда не истекает (имеет бесконечный срок). 
+	//Ввод в существующий ключ должен перезаписать значение
 }
 
-func (receiver) Keys() []string {
+func (k Cache) Keys() []string {
+	var keys []string
+	for key, item := range k.box {
+		if item.timer == 0 || time.Now().Before(item.timer) {
+			keys = append(keys, key)
+		}
+	}
+
+	return keys
+	//должна возвращать часть существующих (не просроченных ключей)
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (k Cache) PutTill(key, value string, deadline time.Time) {
+	k.storage[key] = item{value: value, timer: deadline}
+	//Должен делать то же самое, что и Put, но срок действия пары ключ/значение должен истечь к указанному крайнему сроку.
 }
